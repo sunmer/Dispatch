11e92d3c68a2b702dfd7dc8ae60d69b68afb67ff
Nima Boustanian <nima.boustanian@gmail.com>
2023-11-04 15:01:01 +0100

    unused import

--- frontend/src/contexts/FundraiserContext.tsx
+++ frontend/src/contexts/FundraiserContext.tsx
@@ -2,7 +2,7 @@ import { default as Dispatcher } from '../abi/contracts/Dispatcher.sol/Dispatche
 import React, { createContext, useState, useContext, ReactNode, useEffect } from 'react';
 import { base, polygon } from 'viem/chains'
 import { FundraiserView, ContributorView, PriceData, EventContributionCreated} from '../Interfaces';
-import { Chain, PublicClient, formatEther, getAbiItem, parseAbi } from 'viem';
+import { Chain, PublicClient, formatEther, parseAbi } from 'viem';
 import { useAccount, useNetwork } from 'wagmi';
 import Settings from '../Settings';
 import { OpenIDConnectUserInfo } from '@magic-ext/oauth';
